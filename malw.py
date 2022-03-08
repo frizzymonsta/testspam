05bda1e4608b3e1eafc89dfaf0c205ed172b232e
frizzymonsta <Pe4eHbkO.o@gmail.com>
2022-03-08 21:02:12 +0300

    [+] malw

--- malw.py
+++ malw.py
@@ -5,16 +5,15 @@ class Malware_down:
 
     def spam(self):
         spam = []
-
         for i in range(0, self.ii):
             spam.append(i)
-
         return spam
 
 
 def main():
     s = Malware_down()
-    Malware_down.spam(s)
+    x = Malware_down.spam(s)
+    return x
 
 
 if __name__ == '__main__':
